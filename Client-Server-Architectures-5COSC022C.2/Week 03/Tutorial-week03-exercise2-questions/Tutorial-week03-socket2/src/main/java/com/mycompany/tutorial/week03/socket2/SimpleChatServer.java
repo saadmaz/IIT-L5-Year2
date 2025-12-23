c8ba766317fa2280f3be17fcdf0183cbce83fe60
Saad Mazhar <92235123+saadmazhar03@users.noreply.github.com>
2025-12-23 11:49:01 +0530

    Commit Week 03

---
+++ Client-Server-Architectures-5COSC022C.2/Week 03/Tutorial-week03-exercise2-questions/Tutorial-week03-socket2/src/main/java/com/mycompany/tutorial/week03/socket2/SimpleChatServer.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tutorial.week03.socket2;
+
+//import necessary libraries
+
+
+public class SimpleChatServer {
+    public static void main(String[] args) {
+        try {
+            //Create a new instance of the Server Socket and pass port number
+            
+            //print out a message to say Server is running 
+
+            // Wait for a client to connect and accept the client request
+
+            //print out a message to say client connected and get the IP ddress
+
+            // Input stream to receive messages from the client
+
+            // Output stream to send messages to the client
+
+            
+            //Create a buffer array with type byte, the size must be 1024
+           
+
+            // Read messages from the client and print them
+            
+            
+
+            // Close the sockets
+           
+            
+        //catch IO exception
+        
+    }
+}
