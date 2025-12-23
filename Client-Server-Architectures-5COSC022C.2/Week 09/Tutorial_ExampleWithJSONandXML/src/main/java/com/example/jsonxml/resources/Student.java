f19f285e194a1c40a23e50c3a7f50c71eb8583fe
Saad Mazhar <92235123+saadmazhar03@users.noreply.github.com>
2025-12-23 12:01:30 +0530

    Commit Week 09

---
+++ Client-Server-Architectures-5COSC022C.2/Week 09/Tutorial_ExampleWithJSONandXML/src/main/java/com/example/jsonxml/resources/Student.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.jsonxml.resources;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Student {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+
+    // Default constructor (required by Jackson for deserialization)
+    public Student() {}
+
+    public Student(String id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
